f058fa998b0742c8d0b71d64f61cb6c7b067f960
zomgbat <gavalex7@aol.com>
2024-10-01 15:09:38 +0200

    photos

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,18 +3,18 @@ import '../styles/Gallery.css'
 
 function Gallery() {
     const images = [
-        'https://ourshapes.files.wordpress.com/2023/02/2-1.jpg?w=1024',
-        'https://ourshapes.files.wordpress.com/2023/02/1.jpg',
-        'https://ourshapes.files.wordpress.com/2023/02/dsc_0180.jpg?w=1024&h=576',
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/1-1.jpg?strip=info&w=1920',
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/2-1.jpg?strip=info&w=900',
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/dsc_0180.jpg?strip=info&w=900',
         'https://ourshapes.files.wordpress.com/2023/02/ayr.jpg',
         'https://ourshapes.files.wordpress.com/2023/02/5-4.jpg?w=1024&h=576',
         'https://ourshapes.files.wordpress.com/2023/02/img20220601171105.jpg?w=1024&h=768',
-        'https://ourshapes.files.wordpress.com/2023/02/al-2.webp',
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/dallc2b7e-2024-08-21-11.22.26-a-mystical-old-fashioned-room-with-dark-wood-and-antique-furniture-shelves-filled-with-ancient-books-and-warm-candlelight-casting-a-soft-glow.-a-di.webp',
         'https://ourshapes.files.wordpress.com/2023/02/map2.png',
         'https://ourshapes.files.wordpress.com/2023/02/day-7-2.webp',
-        'https://ourshapes.files.wordpress.com/2023/02/picture3.jpg',
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/unnamed.jpg',
         'https://ourshapes.files.wordpress.com/2023/02/13.webp',
-        'https://ourshapes.files.wordpress.com/2023/02/night-6.webp'
+        'https://yourshapes.wordpress.com/wp-content/uploads/2024/09/a08b0c85-76f8-4b61-8324-0beb92efb112.webp'
     ];
 
     return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -128,12 +128,12 @@ function HomePage() {
                 <section className="dark-background" id="general-information">
                     <article className="container">
                         <h3> Our Content </h3>
-                        <img src="https://yourshapes.wordpress.com/wp-content/uploads/2024/09/odd-eyed_turkish_angora-2.webp?w=600" alt="Laika Inside Sputnik 2" />
+                        <img src="https://yourshapes.wordpress.com/wp-content/uploads/2024/09/screen-shot-2015-03-19-at-15.12.09.png?w=616" alt="Laika Inside Sputnik 2" />
                     <p className="text"> The most fascinating stories on Earth Our main aim is to deliver the most fascinating stories from around the world and beyond, along with illustrating how dark a place the past could be. The idea comes from the website Stories that Shocked the World – my previous employer. Some stories are taken from there but all are written by myself. It is important to consider that many of these stories show inhumanity. 
                         <br></br>
                         <br></br>
                         Although the website is for entertainment purposes, that is used in the sense of learning and sharing. We do not write these stories to make light of any person but to understand their lives. To never take for granted what we have, make the world a better place, and understand how easy it is to return to the darkness of the past. </p>
-                              <img src="https://ourshapes.wordpress.com/wp-content/uploads/2023/01/laika_inside_sputnik2-32.jpg?w=750" alt="Laika Inside Sputnik 2" />
+                              
                    <a className="link-btn" href="/AboutPage"> Learn More </a>
                     </article>
 
